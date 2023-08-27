9111da48534fe8b173d5426339bcdb6600c71bdf
FirdeusGj <firdegjepali@gmail.com>
2023-08-27 16:09:04 +0200

    added server icons

--- src/Components/Servers.jsx
+++ src/Components/Servers.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import './Servers.css'
+import React from "react";
+import "./Servers.css";
 
-export default function Servers({title}) {
+export default function Servers({ title, tooltip }) {
   return (
-    <div>{title}</div>
-  )
+    <div className="servers">
+      {title}
+      <div className="servers-tooltip"><p>{tooltip}</p></div>
+    </div>
+  );
 }
