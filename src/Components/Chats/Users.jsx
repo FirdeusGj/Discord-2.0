87a71d78f8fb964bcaad6cd06d721e46a303f02d
FirdeusGj <firdegjepali@gmail.com>
2023-08-30 10:34:28 +0200

    fixing the users

--- src/Components/Chats/Users.jsx
+++ src/Components/Chats/Users.jsx
@@ -5,11 +5,16 @@ export default function Users() {
     <div className='users'>
         <div className='users-profile'>
             <img className='users-profile-picture' src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" width={32} alt='pfp' />
-            <div className='status'></div>
         </div>
         <div className='users-name'>
             <p>Name</p>
-            <p>x</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 448 512"
+            >
+              <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+            </svg>
         </div>
     </div>
   )
