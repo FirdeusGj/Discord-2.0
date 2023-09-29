3accb4bc73235f312e619192c1805d27019a49be
FirdeusGj <firdegjepali@gmail.com>
2023-09-29 12:26:44 +0200

    designing the chat of users

--- src/Components/mainBody/UserChat.jsx
+++ src/Components/mainBody/UserChat.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import './UserChat.scss'
-
+import "./UserChat.scss";
 export default function UserChat({ usersData }) {
   const { id } = useParams();
-
   const user = usersData.find((user) => user.name.first === id);
-
   return (
-    <div>
+    <div className="userChat__chat-wrapper">
       {user ? (
         <>
-        <div className="userChat__chat">
-          <div>This is {user.name.first}</div>
-          <img src={user.picture.large} height={32} alt={user.name.first} />
-        </div>
+          <div className="userChat__chat">
+            <div className="userChat__features">
+              <div className="userChat__top">
+                <img src={user.picture.large} alt="" height={24} />
+                <p>{user.name.first}</p>
+              </div>
+            </div>
+            <div>
+              <p>This is {user.name.first}</p>
+              <img src={user.picture.large} height={80} alt={user.name.first} />
+            </div>
+          </div>
         </>
       ) : (
         <p>Refresh</p>
